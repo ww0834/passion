676d6b55d890dae5200eef2e4c89bb09fbe1f351
passion <135715751+ww0834@users.noreply.github.com>
2023-07-21 14:48:28 +0800

    更新 quting.js

--- quting.js
+++ quting.js
@@ -2,4 +2,4 @@
 转自大佬
 脚本作者：chxm1023
 ***************************************/
-var body = $response.body.replace(/\"needVerify":\d+/g, '\"needVerify":false').replace(/\"bannerList":\[.+\]/g, '\"bannerList":[]');$done({body});
+var body = $response.body.replace(/\"needVerify":\w+/g, '\"needVerify":false').replace(/\"bannerList":\[.+\]/g, '\"bannerList":[]');$done({body});
