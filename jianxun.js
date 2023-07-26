5d6f35a24e20efc504c28d0a762630d7eda7191d
passion <135715751+ww0834@users.noreply.github.com>
2023-07-26 21:30:01 +0800

    Update jianxun.js

--- jianxun.js
+++ jianxun.js
@@ -1,3 +1,18 @@
+/*
+简讯
+[rewrite_local]
+^https?:\/\/api\.tipsoon\.com/api\/v1\/(user\/info|login\/account) url script-response-body https://raw.githubusercontent.com/ww0834/passion/main/jianxun.js
+[MITM]
+hostname = api.tipsoon.com
+*/
+
+
+
+
+
+
+
+
 var jianxun = JSON.parse($response.body);
 if ($request.url.includes("/user/info")) {
   jianxun.data.is_vip = true;
