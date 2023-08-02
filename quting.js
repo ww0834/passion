ec1af918deb11dfb6bc90cc8701560f9f6edd1e7
passion <135715751+ww0834@users.noreply.github.com>
2023-08-02 18:00:47 +0800

    Update quting.js

--- quting.js
+++ quting.js
@@ -1,2 +1,7 @@
-
-var body = $response.body.replace(/\"bannerList":\[.+\]/g, '\"bannerList":[]');$done({body});
+/*
+[rewrite_local]
+^https?:\/\/(api|api\.next)\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/ww0834/passion/main/quting.js
+[mitm] 
+hostname = api.bspapp.com,api.next.bspapp.com
+*/
+var body = $response.body。replace(/\"bannerList":\[.+\]/g， '\"bannerList":[]');$done({body});
