305978f87a41f074b42bd252f51188168236ffdb
passion <135715751+ww0834@users.noreply.github.com>
2023-08-03 10:42:55 +0800

    Update baiducloud.js

--- baiducloud.js
+++ baiducloud.js
@@ -1,16 +1,29 @@
 
-var baiducloud = JSON.parse($response.body);
-baiducloud = {
+/*
+#转自 https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/BaiduCloud.js
+[rewrite_local]
+
+^https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/BaiduCloud.js
+
+[mitm] 
+
+hostname = pan.baidu.com
+
+*/
+
+let Premium = $response.body;
+var modified = JSON.parse(Premium);
+modified = {
   "product_infos" : [
     {
       "cur_svip_type" : "Crack"，
       "product_name" : "svip2_nd"，
-      "product_description" : "解锁倍速+画质+爱你"，
+      "product_description" : "解锁倍速+画质"，
       "function_num" : 510004015，
       "start_time" : 1672502400，
-      "buy_description" : "不可下载加速"，
+      "buy_description" : "无下载加速"，
       "buy_time" : 980784000，
-      "product_id" : "你好"，
+      "product_id" : "问好"，
       "auto_upgrade_to_svip" : 0，
       "end_time" : 4070880000，
       "cluster" : "vip"，
@@ -22,4 +35,4 @@ baiducloud = {
     "current_level" : 10，
   }
 };
-$done({body:JSON.stringify(baiducloud)});
+$done({body:JSON.stringify(modified)});
