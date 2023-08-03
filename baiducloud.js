7c2b511220681c383bb890de6b40c96fac7ff4a1
passion <135715751+ww0834@users.noreply.github.com>
2023-08-03 14:14:00 +0800

    Update baiducloud.js

--- baiducloud.js
+++ baiducloud.js
@@ -1,4 +1,5 @@
 //https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/BaiduCloud.js
+
 /*
 
 [rewrite_local]
@@ -16,23 +17,23 @@ var modified = JSON.parse(Premium);
 modified = {
   "product_infos" : [
     {
-      "cur_svip_type" : "Crack"，
-      "product_name" : "svip2_nd"，
-      "product_description" : "解锁倍速+画质"，
-      "function_num" : 510004015，
-      "start_time" : 1672502400，
-      "buy_description" : "无下载加速"，
-      "buy_time" : 980784000，
-      "product_id" : "问好"，
-      "auto_upgrade_to_svip" : 0，
-      "end_time" : 4070880000，
-      "cluster" : "vip"，
-      "detail_cluster" : "svip"，
+      "cur_svip_type" : "Crack",
+      "product_name" : "svip2_nd",
+      "product_description" : "解锁倍速+画质",
+      "function_num" : 510004015,
+      "start_time" : 1672502400,
+      "buy_description" : "无下载加速",
+      "buy_time" : 980784000,
+      "product_id" : "问好",
+      "auto_upgrade_to_svip" : 0,
+      "end_time" : 4070880000,
+      "cluster" : "vip",
+      "detail_cluster" : "svip",
       "status" : 0
     }
-  ]，
+  ],
   "level_info" : {
-    "current_level" : 10，
+    "current_level" : 10,
   }
 };
 $done({body:JSON.stringify(modified)});
